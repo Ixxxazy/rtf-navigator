c624fad4ac3f6f9dba5f0e98b97b116945d464f2
Kexogg <kexogg@gmail.com>
2023-04-20 17:52:19 +0500

    Fix Z-index in modal

--- client/src/components/UI/Modal/Modal.tsx
+++ client/src/components/UI/Modal/Modal.tsx
@@ -7,8 +7,8 @@ type Props = {
 };
 const Modal = ({children, visible, setVisible}: Props) => {
     return (
-        <div className={`fixed top-0 bottom-0 right-0 left-0 bg-black/75 ${visible ? 'flex' : 'hidden'}`} onClick={() => setVisible(false)}>
-            <div className={`p-5 m-10 bg-white dark:bg-neutral-700 flex w-full h-auto rounded-xl relative`} onClick={(e) => e.stopPropagation()}>
+        <div className={`z-10 fixed top-0 bottom-0 right-0 left-0 bg-black/75 ${visible ? 'flex' : 'hidden'}`} onClick={() => setVisible(false)}>
+            <div className={`shadow p-5 m-10 bg-white dark:bg-neutral-700 flex w-full h-auto rounded-xl relative`} onClick={(e) => e.stopPropagation()}>
                 <Close onClick={() => setVisible(false)} className={'cursor-pointer absolute right-0 top-0 m-5'} fontSize="large"></Close>
                 {children}
             </div>
