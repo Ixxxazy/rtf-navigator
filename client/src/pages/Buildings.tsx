24d888290988a3692e2e6697dfd3e9c2c50dd561
Kexogg <kexogg@gmail.com>
2023-04-20 17:53:43 +0500

    Add placeholder content

--- client/src/pages/Buildings.tsx
+++ client/src/pages/Buildings.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Buildings = () => {
+    const [buildings, setBuildings] = useState(['ИРИТ-РТФ', 'ФТИ', 'ИНМТ-ХТИ', 'ИСА', 'СП', 'ИнЭУ', 'Теплофак', 'Электрофак', 'Мехфак'])
     return (
-        <div>
-            
-        </div>
+        <section>
+            <h1 className={'text-3xl'}>Институты</h1>
+            <ul>
+                {buildings.map((building) => <li key={building}><h2 className={'text-lg'}>{building}</h2>Вход по пропускам</li>)}
+            </ul>
+        </section>
     );
 };
 
