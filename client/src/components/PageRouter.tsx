1c145ae6fcf81589f81d01f74bab6a7b38454369
Kexogg <kexogg@gmail.com>
2023-04-20 17:51:48 +0500

    Add login page

--- client/src/components/PageRouter.tsx
+++ client/src/components/PageRouter.tsx
@@ -5,6 +5,7 @@ import Error from "../pages/Error";
 import Layout from "./UI/Layout/Layout";
 import Buildings from "../pages/Buildings";
 import About from "../pages/About";
+import Login from "../pages/Login";
 
 const PageRouter = () => {
     return (
@@ -13,6 +14,7 @@ const PageRouter = () => {
                 <Route index element={<BuildingMap />} />
                 <Route path="/buildings" element={<Buildings />}/>
                 <Route path="/about" element={<About />}/>
+                <Route path="/login" element={<Login />} />
                 <Route path="*" element={<Error />} />
             </Route>
         </Routes>
---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import InputBox from "../components/UI/Input/InputBox";
+import Input from "../components/UI/Input/Input";
+import Button from "../components/UI/Button/Button";
+
+const Login = () => {
+    return (
+        <form action="" method="get" className='max-w-md p-3 mx-auto flex flex-col'>
+            <InputBox label='Логин' className='grid grid-cols-3'><Input className='col-span-2' type='text' /></InputBox>
+            <InputBox label='Пароль' className='grid grid-cols-3'><Input className='col-span-2' type='password' /></InputBox>
+            <Button className='w-auto'>Войти</Button>
+        </form>
+    );
+};
+
+export default Login;
