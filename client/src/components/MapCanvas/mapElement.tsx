fe807d5d25e03ee77a7c506e30395a5e43a0c0f4
Kexogg <kexogg@gmail.com>
2023-05-05 04:13:56 +0500

    Remove unused imports

--- client/src/components/MapCanvas/mapElement.tsx
+++ client/src/components/MapCanvas/mapElement.tsx
@@ -1,4 +1,4 @@
-import React, {MouseEvent, useCallback, useContext} from "react";
+import React, {useCallback, useContext} from "react";
 import {BaseMapElement, MapElementTypes} from "./MapElements";
 import {MapContextDispatch} from "./MapContext";
 import {IPoint} from "./Interfaces/Interfaces";
