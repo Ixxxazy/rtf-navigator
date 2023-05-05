4bc062bfaedca4467963f5658b43e767e343f615
Kexogg <kexogg@gmail.com>
2023-05-05 17:45:52 +0500

    Use switch instead of else if

--- client/src/components/MapCanvas/mapElement.tsx
+++ client/src/components/MapCanvas/mapElement.tsx
@@ -12,18 +12,7 @@ type ComponentProps = React.SVGProps<SVGPathElement> & {
 const MapElement = ({element, mousePos, ...props}: ComponentProps) => {
     const dispatch = useContext(MapContextDispatch)
     let d = ''
-    let fill = mousePos ? 'gray' : 'none'
-    if (element.type === MapElementTypes.Waypoint) {
-        let coordinates = element.coordinates[0]
-        d = `M ${coordinates.x - 10},${coordinates.y} a 10,10 0 1,1 20,0 a 10,10 0 1,1 -20,0`
-    } else if (element.type === MapElementTypes.Node) {
-        let coordinates = element.coordinates[0]
-        d = `M ${coordinates.x},${coordinates.y} m 5,0 v 5 h -10 v -10 h 10 v 5`
-    } else if (element.type === MapElementTypes.Geometry) {
-        d = `M ${element.coordinates.map(p => `${p.x},${p.y}`).join(' L ')}${mousePos ? ` L ${mousePos.x},${mousePos.y}` : null}`
-    } else if (element.type === MapElementTypes.Room) {
-        d = `M ${element.coordinates.map(p => `${p.x},${p.y}`).join(' L ')}${mousePos ? ` L ${mousePos.x},${mousePos.y}` : null}`
-    }
+    const fill = mousePos ? 'gray' : 'none'
     const onPathMouseOver = useCallback(() => {
         dispatch({type: ActionType.Changed, element: {id: element.id, color: 'red'}})
     }, [element.id, dispatch]);
@@ -33,7 +22,35 @@ const MapElement = ({element, mousePos, ...props}: ComponentProps) => {
     const onPathContextMenu = useCallback(() => {
         dispatch({type: ActionType.Selected, element: element})
     }, [element, dispatch]);
-    return (<path data-id={element.id} d={d} fill={fill} fillOpacity="50%" {...props} stroke={element.color ?? props.stroke} onMouseOver={onPathMouseOver}
-                  onMouseOut={onPathMouseOut} onContextMenu={onPathContextMenu}/>)
+    switch (element.type) {
+        case MapElementTypes.Door:
+            break
+        case MapElementTypes.Geometry: {
+            d = `M ${element.coordinates.map(p => `${p.x},${p.y}`).join(' L ')}${mousePos ? ` L ${mousePos.x},${mousePos.y}` : null}`
+            break
+        }
+        case MapElementTypes.Node: {
+            let coordinates = element.coordinates[0]
+            d = `M ${coordinates.x},${coordinates.y} m 5,0 v 5 h -10 v -10 h 10 v 5`
+            break
+        }
+        case MapElementTypes.Waypoint: {
+            let coordinates = element.coordinates[0]
+            d = `M ${coordinates.x - 10},${coordinates.y} a 10,10 0 1,1 20,0 a 10,10 0 1,1 -20,0`
+            break
+        }
+        case MapElementTypes.Room: {
+            d = `M ${element.coordinates.map(p => `${p.x},${p.y}`).join(' L ')}${mousePos ? ` L ${mousePos.x},${mousePos.y}` : null}`
+            break
+        }
+        case MapElementTypes.Staircase:
+            break
+        default:
+            throw Error(`Unknown element type ${element.type}`)
+    }
+    return (
+        <path data-id={element.id} d={d} fill={fill} fillOpacity="50%" {...props} stroke={element.color ?? props.stroke}
+              onMouseOver={onPathMouseOver}
+              onMouseOut={onPathMouseOut} onContextMenu={onPathContextMenu}/>)
 };
 export default MapElement
