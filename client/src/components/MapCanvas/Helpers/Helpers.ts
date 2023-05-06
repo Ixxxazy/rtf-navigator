cd5cdb30f6a91fa7906679fc1efa626fbe280150
Kexogg <kexogg@gmail.com>
2023-05-06 05:09:31 +0500

    add getElementCenter

--- client/src/components/MapCanvas/Helpers/Helpers.ts
+++ client/src/components/MapCanvas/Helpers/Helpers.ts
@@ -1,3 +1,5 @@
+import {IPoint} from "../Interfaces/Interfaces";
+
 export const snapTo = (x: number, y: number, s: number) => {
     return ({x: Math.round(x / s) * s, y: Math.round(y / s) * s});
 }
@@ -13,4 +15,16 @@ export const shallowCompare = (obj1: any, obj2: any) =>
     Object.keys(obj1).every(key =>
         obj2.hasOwnProperty(key) && obj1[key] === obj2[key]
     );
+}
+export const getElementCenter = (coordinates: IPoint[]): IPoint => {
+    let x = 0
+    let y = 0
+    let n = coordinates.length
+    if (coordinates.length > 1 && shallowCompare(coordinates[0], coordinates.at(-1)))
+        n -= 1
+    for (let i = 0; i < n ; i++) {
+        x += coordinates[i].x
+        y += coordinates[i].y
+    }
+    return {x: x / n, y: y / n}
 }
