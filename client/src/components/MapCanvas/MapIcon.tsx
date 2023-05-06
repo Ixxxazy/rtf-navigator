04af8e45af810e030af0545d26ad4baee5796ccd
Kexogg <kexogg@gmail.com>
2023-05-06 18:31:55 +0500

    icon change

--- client/src/components/MapCanvas/MapIcon.tsx
+++ client/src/components/MapCanvas/MapIcon.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {IPoint} from "./Interfaces/Interfaces";
+
 type Props = {
     elementCoordinates: IPoint, children: React.ReactNode
 }
 const MapText = ({elementCoordinates, children}: Props) => {
 
         return (
-            <foreignObject alignmentBaseline='middle' x={elementCoordinates.x - 12} y={elementCoordinates.y - 12} width={24} height={24} pointerEvents='none'>
+            <foreignObject alignmentBaseline='middle' x={elementCoordinates.x - 12} y={elementCoordinates.y - 12} width={24}
+                           height={24} pointerEvents='none'>
                 {children}
             </foreignObject>);
     }
