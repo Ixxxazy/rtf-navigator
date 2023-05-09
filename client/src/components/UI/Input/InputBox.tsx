07b7115dfc49f0852cf1c7397efc83d46ca8f544
Kexogg <kexogg@gmail.com>
2023-05-10 04:59:31 +0500

    Add support for icons

--- client/src/components/UI/Input/InputBox.tsx
+++ client/src/components/UI/Input/InputBox.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import {OverridableComponent} from "@mui/material/OverridableComponent";
+import {SvgIconTypeMap} from "@mui/material";
 
 type Props = React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> & {
     label?: string
+    Icon?:  OverridableComponent<SvgIconTypeMap> & { muiName: string }
     children?: React.ReactNode
 };
-const InputBox = ({label, children, ...props}: Props) => {
+const InputBox = ({label, Icon, children, ...props}: Props) => {
     return (
         <label className={`flex items-center h-full shadow bg-neutral-50 dark:bg-neutral-800 rounded-xl m-1 ${props?.className}`}>
-            <span className={`mx-3 min-w-fit font-semibold text-center`}>{label}</span>
+            <span className={`mx-3 min-w-fit font-semibold text-center ${Icon ? 'hidden' : ''}`}>{label}</span>
+            {Icon && <Icon className={`mx-3 min-w-fit font-semibold text-center`}/>}
             {children}
         </label>
     );
