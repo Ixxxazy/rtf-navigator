f344cc414bb517ebe0e871fdf308700972bc3a1b
Kexogg <kexogg@gmail.com>
2023-05-09 16:02:25 +0500

    update map text

--- client/src/components/MapCanvas/MapText.tsx
+++ client/src/components/MapCanvas/MapText.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import {IPoint} from "./Interfaces/Interfaces";
+
 type Props = {
     elementCoordinates: IPoint, text: string
 }
 const MapText = ({elementCoordinates, text}: Props) => {
 
-return (
-    <text x={elementCoordinates.x} y={elementCoordinates.y} style={{textAnchor: 'middle', dominantBaseline: 'middle'}} pointerEvents='none'>
-        {text}
-    </text>);
-}
+        return (
+            <text x={elementCoordinates.x} y={elementCoordinates.y}
+                  style={{textAnchor: 'middle', dominantBaseline: 'middle'}} pointerEvents='none' className='text-3xl dark:fill-white font-bold'>
+                {text}
+            </text>);
+    }
 ;
 
 export default MapText;
