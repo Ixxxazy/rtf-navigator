d81d6d96de5303d7c12205a68a5c93ace84e58bc
Kexogg <kexogg@gmail.com>
2023-05-09 23:28:57 +0500

    formatting

--- client/src/components/MapCanvas/MapText.tsx
+++ client/src/components/MapCanvas/MapText.tsx
@@ -5,7 +5,6 @@ type Props = {
     elementCoordinates: IPoint, text: string
 }
 const MapText = ({elementCoordinates, text}: Props) => {
-
         return (
             <text x={elementCoordinates.x} y={elementCoordinates.y}
                   style={{textAnchor: 'middle', dominantBaseline: 'middle'}} pointerEvents='none' className='text-3xl dark:fill-white font-bold'>
