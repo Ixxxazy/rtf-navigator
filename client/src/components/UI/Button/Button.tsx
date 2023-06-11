28829dab35071d3bc0e0047c070d10f91ae1faff
Kexogg <kexogg@gmail.com>
2023-06-11 16:58:48 +0500

    Changes

--- client/src/components/UI/Button/Button.tsx
+++ client/src/components/UI/Button/Button.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+
 type Props = React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & {
     children?: React.ReactNode
 };
-const Button = ({children, ...props}: Props) =>  {
+const Button = ({children, ...props}: Props) => {
     return (
-        <button {...props} className={`block m-1 p-3 bg-blue-700 hover:bg-blue-600 text-white dark:bg-white dark:hover:bg-neutral-100 dark:text-black rounded-xl align-top transition-colors font-semibold text-lg ${props?.className}`} >
+        <button {...props}
+                className={`block m-1 p-3 bg-blue-700 hover:bg-blue-600 text-white dark:bg-white dark:hover:bg-neutral-100 dark:text-black rounded-xl align-top transition-colors font-semibold text-lg ${props.className}`}>
             {children}
         </button>
     );
--- client/src/components/UI/InfoCard/InfoCard.tsx
+++ client/src/components/UI/InfoCard/InfoCard.tsx
@@ -7,8 +7,10 @@ type Props = React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLD
 
 function InfoCard({children, ...props}: Props) {
     return (
-        <div {...props} className={`p-3 m-3 bg-white dark:bg-neutral-700 rounded-xl h-min shadow ${props?.className}`}>
-            {children}
+        <div className={'m-3'}>
+            <div {...props} className={`p-3 bg-white dark:bg-neutral-700 rounded-xl h-min shadow ${props?.className}`}>
+                {children}
+            </div>
         </div>
     );
 }
