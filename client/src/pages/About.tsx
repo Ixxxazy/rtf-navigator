5580428b27a1bdd67b79bbfe6226409ad481d934
Kexogg <kexogg@gmail.com>
2023-06-11 17:22:42 +0500

    Update about page

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import Logo from "../components/UI/Logo/Logo";
 
 const About = () => {
     return (
-        <article className={'dark:text-white'}>
-            <h1 className={'text-3xl'}>RTF-Navigator</h1>
-            <div>Проектная работа команды</div>
+        <article
+            className='dark:text-white flex flex-col md:flex-row items-center md:items-start md:justify-center gap-5'>
+            <div className='bg-white rounded-full w-1/4 shadow aspect-square flex justify-center items-center'>
+                <Logo className={'w-2/3 h-2/3'}></Logo>
+            </div>
+            <article>
+                <h1 className={'text-3xl'}>RTF-Navigator</h1>
+                <p>
+                    Карты и навигация внутри некоторых корпусов УрФУ.<br/>
+                    Сделано с 💖 в команде <strong>UrFU-Maps</strong><br/>
+                    Github: <a target='_blank' rel="noopener noreferrer"
+                               href='https://github.com/Kexogg/rtf-navigator'>Kexogg/rtf-navigator</a>
+                </p>
+            </article>
         </article>
     );
 };
