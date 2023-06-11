22c38a4a6e7f613175a879d63ff391ce81ef8ac1
Kexogg <kexogg@gmail.com>
2023-06-11 16:58:20 +0500

    Add placeholder buttons

--- client/src/pages/Admin/Admin.tsx
+++ client/src/pages/Admin/Admin.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import Button from "../../components/UI/Button/Button";
 
 const Admin = () => {
     return (
-        <section>
-            <h1>Панель администрирования RTF-Navigator</h1>
-            <Link to='editMap'>Редактировать карту</Link>
+        <section className='flex flex-col'>
+            <h1 className='text-2xl'>Панель администрирования RTF-Navigator</h1>
+            <Button><Link to='editMap'>Редактировать карту</Link></Button>
+            <Button>Редактировать институты</Button>
+            <Button>Редактировать пользователей</Button>
         </section>
     );
 };
