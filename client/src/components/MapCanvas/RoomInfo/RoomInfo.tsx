ec38088004e59c54a0ff028cf3e64e4d590f3b0a
Kexogg <kexogg@gmail.com>
2023-06-11 17:00:16 +0500

    fix null

--- client/src/components/MapCanvas/RoomInfo/RoomInfo.tsx
+++ client/src/components/MapCanvas/RoomInfo/RoomInfo.tsx
@@ -6,13 +6,13 @@ import {MapContext} from "../MapContext";
 const RoomInfo = () => {
     const context = useContext(MapContext)
     return (
-        <InfoCard className={'max-w-xs lg:max-w-md w-full text-left'}>
+        <InfoCard className={'w-full text-left'}>
             {(context.selected && context.selected.type === MapElementTypes.Room) ?
                 <>
                     <h1 className={'text-4xl font-thin'}>{context.selected.name}</h1>
-                    {context.selected!.longName && <h2 className={'text-2xl py-2'}>{context.selected.longName}</h2>}
-                    {context.selected!.workingHours && <h3 className={'text-xl'}>{context.selected.workingHours}</h3>}
-                    {context.selected!.description && <p className='py-3'>{context.selected.description}</p>}
+                    {context.selected.longName && <h2 className={'text-2xl py-2'}>{context.selected.longName}</h2>}
+                    {context.selected.workingHours && <h3 className={'text-xl'}>{context.selected.workingHours}</h3>}
+                    {context.selected.description && <p className='py-3'>{context.selected.description}</p>}
                 </> :
                 <h1 className={'text-4xl font-thin'}>Выберите комнату</h1>
             }
