7a7312cc618829642b65d8b678f7aa1e3873ceba
Kexogg <kexogg@gmail.com>
2023-06-12 23:24:19 +0500

    Update layout

--- client/src/pages/Admin/EditMap.tsx
+++ client/src/pages/Admin/EditMap.tsx
@@ -1,21 +1,9 @@
 import React from 'react';
 import MapCanvas from "../../components/MapCanvas/MapCanvas";
-import InputBox from "../../components/UI/Input/InputBox";
-import Dropdown from "../../components/UI/Dropdown/Dropdown";
 
 const EditMap = () => {
     return (
-        <section>
-            <div className={'flex flex-row flex-wrap items-center gap-3'}>
-                <InputBox label='Институт'>
-                    <Dropdown options={['ИРИТ-РТФ', 'ХТИ']} />
-                </InputBox>
-                <InputBox label='Этаж'>
-                    <Dropdown options={['1','2','3']} />
-                </InputBox>
-            </div>
-            <MapCanvas className={'w-full'} editingAllowed={true} />
-        </section>
+        <MapCanvas className={'w-full'} editingAllowed={true}/>
     );
 };
 
