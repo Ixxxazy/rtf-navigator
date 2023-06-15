005e93fab309e51985917539579e9ba4684b564c
Kexogg <kexogg@gmail.com>
2023-06-16 03:32:47 +0500

    Fix styles

--- client/src/components/UI/InfoCard/InfoCard.tsx
+++ client/src/components/UI/InfoCard/InfoCard.tsx
@@ -7,11 +7,11 @@ type Props = React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLD
 
 function InfoCard({children, ...props}: Props) {
     return (
-        <div className={'m-3'}>
-            <div {...props} className={`p-3 bg-white dark:bg-neutral-700 rounded-xl h-min shadow ${props?.className}`}>
-                {children}
+            <div {...props} className={`p-3 ${props?.className ?? ''}`}>
+                <div className={'p-3 bg-white dark:bg-neutral-700 rounded-xl h-full shadow'}>
+                    {children}
+                </div>
             </div>
-        </div>
     );
 }
 
