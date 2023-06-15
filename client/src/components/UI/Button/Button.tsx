52f181607227159f2a876987f880e5e8588b0f59
Kexogg <kexogg@gmail.com>
2023-06-16 04:13:12 +0500

    Fix styles

--- client/src/components/UI/Button/Button.tsx
+++ client/src/components/UI/Button/Button.tsx
@@ -5,10 +5,11 @@ type Props = React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElemen
 };
 const Button = ({children, ...props}: Props) => {
     return (
-        <button {...props}
-                className={`block m-1 p-3 bg-blue-700 hover:bg-blue-600 text-white dark:bg-white dark:hover:bg-neutral-100 dark:text-black rounded-xl align-top transition-colors font-semibold text-lg ${props.className}`}>
-            {children}
-        </button>
+            <button {...props}
+                    className={`block my-1 p-3 bg-blue-700 hover:bg-blue-600 text-white dark:bg-white dark:hover:bg-neutral-100 dark:text-black rounded-xl align-top transition-colors font-semibold text-lg ${props.className}`}>
+                {children}
+            </button>
+
     );
 };
 
